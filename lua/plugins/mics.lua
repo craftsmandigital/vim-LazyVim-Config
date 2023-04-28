311670a0366342439c9293b298e4ec1a281cacbd
craftsmandigital <hackjack@tutanota.com>
2023-04-28 20:26:05 +0200

    Tmux plugin and keymap to handle tmuxnavigation when Nvim is active

---
+++ lua/plugins/mics.lua
@@ -0,0 +1,11 @@
+-- mics plugins
+
+return {
+  -- tmux vim
+
+  -- {"alexghergh/nvim-tmux-navigation"},
+  { "christoomey/vim-tmux-navigator" },
+  -- markdown support
+  { "godlygeek/tabular" }, -- required by vim-markdown
+  { "plasticboy/vim-markdown" },
+}
