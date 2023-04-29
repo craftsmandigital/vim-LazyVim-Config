ec3bb59daedb458600f0b84e10094576c9c161c9
craftsmandigital <hackjack@tutanota.com>
2023-04-29 15:17:17 +0200

    Added git project suport. LazyVim remembers my projects

--- lua/plugins/mics.lua
+++ lua/plugins/mics.lua
@@ -8,4 +8,16 @@ return {
   -- markdown support
   { "godlygeek/tabular" }, -- required by vim-markdown
   { "plasticboy/vim-markdown" },
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {},
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      require("telescope").load_extension("projects")
+    end,
+    keys = {
+      { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+    },
+  }
 }
