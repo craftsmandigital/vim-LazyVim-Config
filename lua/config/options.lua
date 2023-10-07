ceca9a4e0bafb1a21e22cba50ce8cc10330cd02e
craftsmandigital <hackjack@tutanota.com>
2023-10-07 12:44:58 +0200

    config of clang stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- opts = function(_, opts)
+--   if type(opts.ensure_installed) == "table" then
+--     vim.list_extend(opts.ensure_installed, { "c", "cpp" })
+--   end
+-- end
+vim.opt.swapfile = false
